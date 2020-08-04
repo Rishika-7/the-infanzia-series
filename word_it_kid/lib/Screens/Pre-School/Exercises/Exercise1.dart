7bce6271c07ba20501834dcd67fe6ef761b2dda6
Akanksha Pant <pantakshi1999@gmail.com>
2020-08-04 17:07:38 +0530

    First Exercise added

---
+++ word_it_kid/lib/Screens/Pre-School/Exercises/Exercise1.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'DragginMech.dart';
+import 'dart:math';
+import 'RandomPairGenerator.dart';
+
+
+// If you want to navigate to the Exercise page use the code given below.
+// You are required to give the specified value to min and max parameters.
+//Code
+// onPressed: (){
+//Navigator.of(context).push(new MaterialPageRoute(
+//builder: (BuildContext context) =>Exercise(
+//max: 5,
+//min: 0,
+//)));
+
+class Exercise extends StatefulWidget {
+  @override
+  int max ;
+  int min ;
+  Exercise({this.min, this.max});
+  _ExerciseState createState() => _ExerciseState(
+    max: max,
+    min: min,
+  );
+}
+
+class _ExerciseState extends State<Exercise> {
+  int seed = 5;
+  List<Widget> alpha = new List();
+  List<Widget> targets = new List();
+  static Random rand = new Random();
+   int max ;
+   int min ;
+  _ExerciseState({this.max, this.min});
+  buildList() async {
+    for(int i = 0; i < 5;i++){
+      var element = AlphaList[min + rand.nextInt(max - min)];
+      alpha.add(drag(
+        alphabet: element["alpha"],
+      ));
+      targets.add(Target(
+        alphabet: element["alpha"],
+        imgURL: element["I"],
+      ));
+      max += 5;
+      min += 5;
+    }
+  }
+  void initState(){
+    buildList();
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+        body: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("Images/seasons/summer-bg.png"),
+                fit: BoxFit.fill,
+              )
+          ),
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Container(
+                    height: 100,
+                    width: 100,
+                  )
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    flex: 5,
+                    child: Column(
+                      children: alpha..shuffle(Random(seed)),
+                    ),),
+                  Expanded(
+                    flex: 5,
+                    child: Column(
+                        children:targets
+                    ),),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  FloatingActionButton(
+                    child: Icon(Icons.refresh),
+                    onPressed: (){
+                      setState(() {
+                        seed++;
+                        max = 5;
+                        min = 0;
+                        Navigator.of(context).pop(new MaterialPageRoute(
+                            builder: (BuildContext context) =>Exercise()));
+                        Navigator.of(context).push(new MaterialPageRoute(
+                            builder: (BuildContext context) =>Exercise(
+                              min: 0,
+                              max: 5,
+                            )));
+                      });
+                    },
+                  )
+
+                ],
+              )
+            ],
+          )
+        )
+    );
+  }
+}
+
+
---
+++ word_it_kid/lib/Screens/Pre-School/Exercises/RandomPairGenerator.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:math';
+
+
+var AlphaList=[
+  {"alpha" : "A", "I" : "Images/ant.png"},
+  {"alpha" : "B", "I" : "Images/Bee.png"},
+  {"alpha" : "C", "I" : "Images/cat.png"},
+  {"alpha" : "D", "I" : "Images/dog.png"},
+  {"alpha" : "E", "I" : "Images/eagle.png"},
+  {"alpha" : "F",  "I" : "Images/fish.png"},
+  {"alpha" : "G",  "I" : "Images/girrafe.png"},
+  {"alpha" : "H",  "I" : "Images/Horse.png"},
+  {"alpha" : "I",  "I" : "Images/igloo.png"},
+  {"alpha" : "J",  "I" : "Images/Jackal.png"},
+  {"alpha" : "K",  "I" : "Images/Kangaroo.png"},
+  {"alpha" : "L",  "I" : "Images/lion.png"},
+  {"alpha" : "M",  "I" : "Images/monkey.png"},
+  {"alpha" : "N",  "I" : "Images/nest.png"},
+  {"alpha" : "O",  "I" : "Images/owl.png"},
+  {"alpha" : "P",  "I" : "Images/peacock.png"},
+  {"alpha" : "Q",  "I" : "Images/queen.png"},
+  {"alpha" : "R",  "I" : "Images/rat.png"},
+  {"alpha" : "S",  "I" : "Images/snake.png"},
+  {"alpha" : "T",  "I" : "Images/toy.png"},
+  {"alpha" : "U",  "I" : "Images/Umbrella.png"},
+  {"alpha" : "V",  "I" : "Images/violin.png"},
+  {"alpha" : "W",  "I" : "Images/watch.png"},
+  {"alpha" : "X",  "I" : "Images/xylophone.png"},
+  {"alpha" : "Y",  "I" : "Images/yak.png"},
+  {"alpha" : "Z",  "I" : "Images/zebra.png"},
+
+];
+
+

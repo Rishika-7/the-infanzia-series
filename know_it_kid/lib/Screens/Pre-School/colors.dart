1157eca72644d980d3e55eb3f58df8583ecb7420
Rishika-7 <rishikajain028@gmail.com>
2020-08-28 00:37:01 +0530

    Modified Background Contrast

--- know_it_kid/lib/Screens/Pre-School/colors.dart
+++ know_it_kid/lib/Screens/Pre-School/colors.dart
@@ -90,7 +90,7 @@ class _ColorsState extends State<Colours> {
         height: MediaQuery.of(context).size.height,
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage("Images/BodypartsBG.png"),
+            image: AssetImage("Images/ColorsBG.png"),
             fit: BoxFit.fill,
           ),
         ),

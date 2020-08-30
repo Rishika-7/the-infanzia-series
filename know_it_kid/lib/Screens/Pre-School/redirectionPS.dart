db9bee9f15762cb284405b00e6ca0618a0fd34fd
sonalsk <sonalkushwaha039@gmail.com>
2020-08-30 23:33:39 +0530

    Resized components of Redirection Page in Know it Kid

--- know_it_kid/lib/Screens/Pre-School/redirectionPS.dart
+++ know_it_kid/lib/Screens/Pre-School/redirectionPS.dart
@@ -6,149 +6,156 @@ import 'package:knowitkid/Screens/Pre-School/levelsPS.dart';
 import 'package:knowitkid/Screens/Rhymes/rhymes.dart';
 
 
-
-
 class PreSchool extends StatelessWidget {
+
+  final String name;
+  final String age;
+
+  PreSchool({Key key, this.name, this.age}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      debugShowCheckedModeBanner: false,
+      
       home: Scaffold(
         body: Container(
           child: Container(
             child: Column(
               children: <Widget>[
-                Expanded (
-                  flex: 3,
-                  child : Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: <Widget>[
-                      Container(
-                        alignment: Alignment.bottomRight,
-                        child : RaisedButton(
-                          onPressed: () {
-                            debugPrint('Clicked');
-                            Navigator.push(context, MaterialPageRoute(builder: (context){
-                              return PSLevels();
-                            })
-                            );
-                          },
 
-                          color: Colors.deepOrange,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20)
-                          ) ,
-                          child: Row(
-                            children: <Widget>[
-                              Image.asset(
-                                "Images/LearnSymbol.png",
-                                width: 30,
-                                height: 30,
-                              ),
-                              Text(
-                                "  LEARN",
-                                style: TextStyle(
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 18
-                                ),
-                              ),
+                SizedBox(
+                  height: 200,
+                ),
 
-                            ],
+                Container(
+                  margin: EdgeInsets.only(right: 40, top: 10),
+                  alignment: Alignment.bottomRight,
+                  child: SizedBox(
+                    height: 50,
+                    width: 150,
+                    child: RaisedButton(
+                      onPressed: () {
+                        debugPrint('Clicked');
+                        Navigator.push(context, MaterialPageRoute(builder: (context){
+                          return PSLevels();
+                        })
+                        );
+                      },
+
+                      color: Colors.deepOrange,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)
+                      ),
+
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Image.asset(
+                            "Images/LearnSymbol.png",
+                            width: 30,
+                            height: 30,
                           ),
-                        ),
-                      ) ],
+                          Text(
+                            "LEARN",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                 ),
 
-                Expanded(
-                  flex: 3,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: <Widget>[
-                      Container(
-                          alignment: Alignment.center,
-                          child :RaisedButton(
-                              onPressed: () {
-                                debugPrint('Clicked');
-                                Navigator.push(context, MaterialPageRoute(builder: (context){
-                                  return Rhyme();
-                                })
-                                );
-                              },
-                              color: Colors.deepOrange,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(20)
-                              ) ,
-                              child: Row(
-                                children: <Widget>[
-                                  Image.asset("Images/SingSymbol.png",
-                                    width: 30,
-                                    height: 30,
-                                    alignment: Alignment.bottomLeft,),
-                                  Text(
-                                    "SING",
-                                    style: TextStyle(
-                                        color: Colors.black,
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 18
-                                    ),
-                                  ),
+                Container(
+                  margin: EdgeInsets.only(top: 60),
+                  alignment: Alignment.center,
+                  child: SizedBox(
+                    height: 50,
+                    width: 150,
+                    child: RaisedButton(
+                      onPressed: () {
+                        debugPrint('Clicked');
+                        Navigator.push(context, MaterialPageRoute(builder: (context){
+                          return Rhyme();
+                        })
+                        );
+                      },
+                      color: Colors.deepOrange,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Image.asset("Images/SingSymbol.png",
+                            width: 30,
+                            height: 30,
+                            alignment: Alignment.bottomLeft,),
+                          Text(
+                            "SING",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
 
-                                ],
-                              ))
-                      )],
+                        ],
+                      ),
+                    ),
                   ),
                 ),
 
-                Expanded (
-                  flex: 4,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: <Widget>[
-                      Container(
-                          alignment: Alignment.topRight,
-                          child :RaisedButton(
-                              onPressed: () {
-                                debugPrint('Clicked');
-                                Navigator.push(context, MaterialPageRoute(builder: (context){
-                                  return Game();
-                                })
-                                );
-                              },
+                Container(
+                  margin: EdgeInsets.only(right: 40, top: 60),
+                  alignment: Alignment.bottomRight,
+                  child: SizedBox(
+                    height: 50,
+                    width: 150,
+                    child: RaisedButton(
+                      onPressed: () {
+                        debugPrint('Clicked');
+                        Navigator.push(context, MaterialPageRoute(builder: (context){
+                          return Game();
+                        })
+                        );
+                      },
 
-                              color: Colors.deepOrange,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(20)
-                              ) ,
-                              child: Row(
-                                children: <Widget>[
-                                  Image.asset(
-                                    "Images/GamesSymbol.png",
-                                    width: 20,
-                                    height: 20,
-                                  ),
-                                  Text(
-                                    "  PLAY",
-                                    style: TextStyle(
-                                        color: Colors.black,
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 18
-                                    ),
-                                  ),
-                                ],
-                              ))
-                      ) ],
+                      color: Colors.deepOrange,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)
+                      ),
 
-                  ),)
-
-              ], ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Image.asset(
+                            "Images/GamesSymbol.png",
+                            width: 20,
+                            height: 20,
+                          ),
+                          Text(
+                            "PLAY",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
 
             constraints: BoxConstraints.expand(),
             decoration: BoxDecoration(
